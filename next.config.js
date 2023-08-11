1295fa43929ad74e09c702629833554a03682e9c
HarshTondak <harshtondak007@gmail.com>
2023-08-11 13:23:46 +0530

    img file transfer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    // Add the address of storage for images...
+    domains: ["localhost"],
+  },
 };
 
 module.exports = nextConfig;
--- src/components/Chat/MessageBar.jsx
+++ src/components/Chat/MessageBar.jsx
@@ -1,6 +1,6 @@
 import { useStateProvider } from "@/context/StateContext";
 import { reducerCases } from "@/context/constants";
-import { ADD_MESSAGE_ROUTE } from "@/utils/ApiRoutes";
+import { ADD_IMAGE_MESSAGE_ROUTE, ADD_MESSAGE_ROUTE } from "@/utils/ApiRoutes";
 import axios from "axios";
 import EmojiPicker from "emoji-picker-react";
 import React, { useEffect, useRef, useState } from "react";
@@ -8,12 +8,51 @@ import { BsEmojiSmile } from "react-icons/bs";
 import { FaMicrophone } from "react-icons/fa";
 import { ImAttachment } from "react-icons/im";
 import { MdSend } from "react-icons/md";
+import PhotoPicker from "../common/PhotoPicker";
 
 function MessageBar() {
   const [{ userInfo, currentChatUser, socket }, dispatch] = useStateProvider();
   const [message, setMessage] = useState("");
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const emojiPickerRef = useRef(null);
+  // To set the uploaded photo
+  const [grabPhoto, setGrabPhoto] = useState(false);
+
+  const photoPickerChange = async (e) => {
+    try {
+      const file = e.target.files[0];
+      const formData = new FormData();
+      formData.append("image", file);
+
+      const response = await axios.post(ADD_IMAGE_MESSAGE_ROUTE, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        params: {
+          from: userInfo.id,
+          to: currentChatUser.id,
+        },
+      });
+
+      if (response.status === 201) {
+        socket.current.emit("send-msg", {
+          to: currentChatUser?.id,
+          from: userInfo?.id,
+          message: response?.data?.message,
+        });
+        dispatch({
+          type: reducerCases.ADD_MESSAGE,
+          newMessage: {
+            ...response.data.message,
+          },
+          // For identifying the message from current logged in user
+          fromSelf: true,
+        });
+      }
+    } catch (error) {
+      console.lof(error);
+    }
+  };
 
   useEffect(() => {
     const handleOutsideClick = (event) => {
@@ -67,6 +106,18 @@ function MessageBar() {
     }
   };
 
+  useEffect(() => {
+    if (grabPhoto) {
+      const data = document.getElementById("photo-picker");
+      data.click();
+      document.body.onfocus = (e) => {
+        setTimeout(() => {
+          setGrabPhoto(false);
+        }, 1000);
+      };
+    }
+  }, [grabPhoto]);
+
   return (
     <div className="bg-panel-header-background h-20 px-4 flex items-center gap-6 relative">
       <>
@@ -88,6 +139,7 @@ function MessageBar() {
           <ImAttachment
             className="text-panel-header-icon cursor-pointer text-xl"
             title="Attach File"
+            onClick={() => setGrabPhoto(true)}
           />
         </div>
 
@@ -113,6 +165,7 @@ function MessageBar() {
           /> */}
         </div>
       </>
+      {grabPhoto && <PhotoPicker onChange={photoPickerChange} />}
     </div>
   );
 }
